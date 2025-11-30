1bf6293c82f2e4fcc73faf4bb1a3cd7cc5aabdb3
valeriaalvarez66 <cresposvale@gmail.com>
2025-11-29 21:27:20 -0500

    .--

--- app.py
+++ app.py
@@ -2,31 +2,26 @@ from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
 import json
+from flask_cors import CORS  # 🔥 IMPORTANTE
 
 app = Flask(__name__)
+CORS(app)  # 🔥 PERMITE PETICIONES DESDE TU FRONTEND
 
-# Cargar modelo
 modelo = joblib.load("models/modelo_zoo.pkl")
 
-# Cargar columnas
 with open("json/columnas.json", "r") as f:
     columnas = json.load(f)
 
-# Ruta principal → Formulario
 @app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
 
-# Ruta predicción
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-
     entrada = [data.get(col, 0) for col in columnas]
     entrada = np.array(entrada).reshape(1, -1)
-
     prediccion = modelo.predict(entrada)[0]
-
     return jsonify({"prediccion": int(prediccion)})
 
 if __name__ == "__main__":
